d0e7410981050d0ae63053a3d866d6baf3473765
e-leal <leal.elyssa03@gmail.com>
2020-09-19 22:22:50 -0500

    add user router for CRUD calls

---
+++ routes/user-routes.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+const {
+    getAllUser,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser
+} = require('../../controllers/user-controller');
+const { updateUser } = require('../controllers/user-controller');
+
+// set up GET all and POST 
+router
+    .route('/')
+    .get(getAllUser)
+    .post(createUser);
+
+// set up GET one, PUT, and DELETE 
+router
+    .route('/:id')
+    .get(getUserById)
+    .put(updateUser)
+    .delete(deleteUser);
+
+module.exports = router;
