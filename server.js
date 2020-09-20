787ffe1a80d11ea6cce750bf0f9f2c42d8685013
e-leal <leal.elyssa03@gmail.com>
2020-09-19 22:55:51 -0500

    Updated index routes and server.js file with additional routing information

--- server.js
+++ server.js
@@ -1,4 +1,14 @@
 const mongoose = require('mongoose');
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3001
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+app.use(require('./routes'));
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-not-work',{
     useFindAndModify: false,
@@ -7,3 +17,4 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-not-work
 });
 
 mongoose.set('debug', true);
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
